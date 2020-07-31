fde626f93597de7fe1aebed8d10da5572a72ca1a
Alex Gubskyy <aleks.gubskyy@gmail.com>
2020-07-31 19:53:13 +0200

    fix: app

--- src/components/Disconnect.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Disconnect(props) {
-  if (props.stance === "disconnected") {
-    return <h4> Connection lost :(</h4>;
-  } else return null;
-}
-
-export default Disconnect;
